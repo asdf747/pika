97c83020b88a89422e60fef1bc18d0fc11372d45
confh <88879804+confh@users.noreply.github.com>
2021-08-17 08:30:01 +0200

    Update guildMemberUpdate.js

--- eventss/guildMemberUpdate.js
+++ eventss/guildMemberUpdate.js
@@ -1,6 +1,6 @@
 module.exports = async (client, oldMember, newMember) => {
 if(newMember.guild.id === '655780171496030240' && !oldMember.roles.cache.some(r => r.id === '783924903061618728') && newMember.roles.cache.some(r => r.id === '783924903061618728')){
-    const role = newMember.guild.roles.cache.some('789432483166945300')
+    const role = newMember.guild.roles.cache.get('789432483166945300')
     newMember.roles.add(role, 'Got the 25M donator role.')
 }
 
