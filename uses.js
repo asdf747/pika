08e0c3118cccba161badd80c91bc437bf26e2339
confh <88879804+confh@users.noreply.github.com>
2021-08-19 01:02:23 +0200

    Update uses.js

--- uses.js
+++ uses.js
@@ -1,7 +1,7 @@
 async function pp(client, message, arguments, economy){
     await economy.findOne({ id: message.author.id }, async(err, data) => {
         if(data){
-            data.Inventory.update({ Name: "PP" }, { $inc: {Count: -1} })
+            data.Inventory.updateOne({ Name: "PP" }, { $inc: {Count: -1} })
             return `You just lost one pp lolololl`
         }
     })
