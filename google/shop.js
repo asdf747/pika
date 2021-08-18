f06d7d13144332c296c1ee150a6a3a9420ef418b
confh <88879804+confh@users.noreply.github.com>
2021-08-18 18:46:41 +0200

    lol

--- google/shop.js
+++ google/shop.js
@@ -1,9 +1,21 @@
 const shop = require('../shop.json')
 const Pagination = require('discord-paginationembed')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     commands: 'shop',
     callback: async(message, arguments, text, client) => {
+        if(arguments[0]){
+            let item = shop.find(item => item.ID.includes(arguments[0].toLowerCase()))
+            if(!item) return message.channel.send("This item doens't exist.")
+            message.channel.send(
+                new MessageEmbed()
+                .setTitle(`${item.Name}`)
+                .setDescription(`${item.Description}\n\n**Buy:** ${item.Price}\n**Sell:** ${item.Sell}`)
+                .setThumbnail(item.Image)
+            )
+            return
+        }
         let items = shop.filter(item => item.Shop).map(item => `**${item.Emoji} ${item.Name}** - ${item.Price}\n${item.Description}\n`)
         if(!items.length) items = ["Empty"]
     const FieldsEmbed = new Pagination.FieldsEmbed()
