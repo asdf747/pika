77ef46f59b3ced172a23ca9ea2adf4b106c917a6
confh <88879804+confh@users.noreply.github.com>
2021-08-19 00:24:38 +0200

    Update balance.js

--- google/balance.js
+++ google/balance.js
@@ -13,6 +13,8 @@ module.exports = {
           new MessageEmbed()
           .setTitle(`${user.username}'s balance`)
           .setDescription(`**Wallet:** ${parseInt(data.Wallet).toLocaleString("en-US")}\n**Bank:** ${parseInt(data.InBank).toLocaleString("en-US")}/${parseInt(data.Bank).toLocaleString("en-US")}`)
+          .setTimestamp()
+          .setColor("BLUE")
         )
       } else if(!data){
         await new bal({
@@ -26,6 +28,8 @@ module.exports = {
           new MessageEmbed()
           .setTitle(`${user.username}'s balance`)
           .setDescription(`**Wallet:** 500\n**Bank:** 0/100`)
+          .setTimestamp()
+          .setColor("BLUE")
         )
 
       }
