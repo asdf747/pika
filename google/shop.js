416cdd882c7c7524ede4ab29baa777420e5aae46
confh <88879804+confh@users.noreply.github.com>
2021-08-19 16:43:35 +0200

    Update shop.js

--- google/shop.js
+++ google/shop.js
@@ -12,7 +12,7 @@ module.exports = {
             let finding = await economy.findOne({ id: message.author.id })
             let counter = 0
             if(finding && finding.Inventory.length){
-                counting = await finding.Inventory.find(ite => ite.Name.toLowerCase() === item.Name.toLowerCase())
+                counting = finding.Inventory.find(ite => ite.Name.toLowerCase() === item.Name.toLowerCase())
                 counter = counting.Count
             }
             message.channel.send(
