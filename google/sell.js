c8b1dd10c2d04f5758baf4a16a12e07cdea43fb2
confh <88879804+confh@users.noreply.github.com>
2021-08-19 09:28:23 +0200

    Update sell.js

--- google/sell.js
+++ google/sell.js
@@ -16,6 +16,7 @@ module.exports = {
         if(!doc.Inventory.find(item => item.Name.toLowerCase() === item.Name.toLowerCase()) || doc.Inventory.find(item => item.Name.toLowerCase() === item.Name.toLowerCase()).Count < 1) return message.channel.send("You don't own this item.")
         if(item.Sell === "???") return message.channel.send("You can't sell this item")
         let amount = 1
+        if(arguments[1] && Number(arguments[1])) amount = Number(arguments[1])
         let money = item.Sell * amount
         if(doc.Inventory.find(item => item.Name.toLowerCase() === item.Name.toLowerCase()).Count < amount) return message.channel.send(`You don't have this many of **${item.Name}**`)
         await economy.findOneAndUpdate({ id: message.author.id }, { $inc: {Wallet: money} })
