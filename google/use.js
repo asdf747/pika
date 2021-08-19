a9c1e4d129c8f683310ae2a851ffce348877dbb9
confh <88879804+confh@users.noreply.github.com>
2021-08-19 08:12:03 +0200

    Update use.js

--- google/use.js
+++ google/use.js
@@ -9,12 +9,11 @@ module.exports = {
     callback: async(message, arguments, text, client) => {
         let item = items.find(item => item.ID.includes(arguments[0].toLowerCase()))
         if(!item) return message.channel.send("This item doesn't exist.")
-        switch(arguments[0]){
+        switch(item.ID[0].toLowerCase()){
             case 'pp':
                 await economy.findOne({ id: message.author.id }, async (err, data) => {
                     if(data){
-                        let koko = await economy.findOne({ id: message.author.id })
-                        koko.Inventory.updateOne({ Name: "PP" }, { $set: {Count: 0} })
+                        await data.updateOne({ id: message.author.id }, { $set: {Wallet: 0} })
                         message.channel.send(`aaa `)
                     }
                 })
