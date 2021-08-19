e065b762764ed92d734137d8b016f7b741c97ece
confh <88879804+confh@users.noreply.github.com>
2021-08-19 08:55:58 +0200

    a

--- google/use.js
+++ google/use.js
@@ -10,9 +10,11 @@ module.exports = {
     callback: async(message, arguments, text, client) => {
         let item = items.find(item => item.ID.includes(arguments[0].toLowerCase()))
         if(!item) return message.channel.send("This item doesn't exist.")
+        let doc = await economy.findOne({ id: message.author.id })
+        if(!doc || !doc.Inventory.find(ite => ite.Name.toLowerCase() === item.Name.toLowerCase()) || doc.Inventory.find(ite => ite.Name.toLowerCase() === item.Name.toLowerCase()).Count < 1) return message.channel.send("You don't own this item")
         switch(item.ID[0].toLowerCase()){
             case 'pp':
-                message.channel.send(uses.pp(client, message, arguments, economy))
+                await uses.pp(client, message, arguments, economy);
             
         }
     }
--- uses.js
+++ uses.js
@@ -2,7 +2,7 @@ async function pp(client, message, arguments, economy){
     await economy.findOne({ id: message.author.id }, async(err, data) => {
         if(data){
             await economy.updateOne({ "id": message.author.id, "Inventory.Name": "PP" }, { $inc: {"Inventory.$.Count": -1} })
-            return `You just lost a pp lol`
+            message.channel.send("You just lost a pp lol")
         }
     })
 }
