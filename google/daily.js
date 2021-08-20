3d28296045086b69929100f45eef6f4b98ac33fa
confh <88879804+confh@users.noreply.github.com>
2021-08-20 09:03:15 +0200

    a

--- google/daily.js
+++ google/daily.js
@@ -9,16 +9,14 @@ module.exports = {
     callback: async(message, arguments, text, client) => {
         const db = new Database(client.db)
         let bonus = await db.fetch(`bonus_daily_${message.author.id}`)
-        if(!bonus) await db.set(`bonus_daily_${message.author.id}`, 0)
-        let lastdaily = await db.fetch(`daily_${message.author.id}`)
-        let streak = await db.fetch(`daily_streak_${message.author.id}`)
-        if(!streak) streak = 0
-        if(streak !== null && moment.duration(Date.now() - lastdaily).as('days') >= 2) await db.set(`daily_streak_${message.author.id}`, 0)
-        
-        if(bonus !== null && moment.duration(Date.now() - lastdaily).as('days') >= 2) await db.set(`bonus_daily_${message.author.id}`, 0)
+        if(!bonus) await db.set(`bonus_daily_${message.author.id}`, 1)
+        let lastwork = await db.fetch(`last_daily_${message.author.id}`)
+        if(bonus !== null && moment.duration(Date.now() - lastdaily).as('days') < 2) await db.add(`bonus_daily_${message.author.id}`, 2000)
+                await db.add(`daily_streak_${message.author.id}`, 1)
+        if(bonus !== null && moment.duration(Date.now() - lastwork).as('days') < 2) await db.add(`bonus_daily_${message.author.id}`, 1)
+        if(bonus !== null && moment.duration(Date.now() - lastwork).as('days') >= 2) await db.set(`bonus_daily_${message.author.id}`, 1)
         
         bonus = await db.fetch(`bonus_daily_${message.author.id}`)
-        streak = await db.fetch(`daily_streak_${message.author.id}`)
         let amount = 10000 + bonus
         await economy.findOne({ id: message.author.id }, async(err, data) => {
             if(data){
@@ -28,11 +26,11 @@ module.exports = {
                     new MessageEmbed()
                     .setTitle("Claimed daily")
                     .setDescription(`You've claimed your daily and got **${amount.toLocaleString("en-US")}**`)
-                    .setFooter(`Streak: ${streak} (+${bonus})`)
+                    .setFooter(`Streak: ${bonus / 2000} (+${bonus})`)
                     .setColor("BLUE")
                 )
-                if(bonus !== null && moment.duration(Date.now() - lastdaily).as('days') < 2) await db.add(`bonus_daily_${message.author.id}`, 2000)
-                await db.add(`daily_streak_${message.author.id}`, 1)
+                await db.set(`last_daily_${message.author.id}`, Date.now())
+
             }if(!data){
                 await new economy({
                     id: message.author.id,
@@ -46,11 +44,10 @@ module.exports = {
                         new MessageEmbed()
                         .setTitle("Claimed daily")
                         .setDescription(`You've claimed your daily and got **${amount.toLocaleString("en-US")}**`)
-                        .setFooter(`Streak: ${streak} (+${bonus})`)
+                        .setFooter(`Streak: ${bonus / 2000} (+${bonus})`)
                         .setColor("BLUE")
                     )
-                    if(bonus !== null && moment.duration(Date.now() - lastdaily).as('days') < 2) await db.add(`bonus_daily_${message.author.id}`, 2000)
-                    await db.add(`daily_streak_${message.author.id}`, 1)
+                    await db.set(`last_daily_${message.author.id}`, Date.now())
                 })
             }
         })
