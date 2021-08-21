84037a8b7fbbebd2153263eee1d9b68eb930b64f
confh <88879804+confh@users.noreply.github.com>
2021-08-21 15:28:10 +0200

    Update share.js

--- economyEvents/share.js
+++ economyEvents/share.js
@@ -1,6 +1,9 @@
 const funcs = require('../funcs')
+const EventEmitter = require('events')
+const e = new EventEmitter()
 
-client.economy.on('share', async function (giver, gived, amount){
+
+e.economy.on('share', async function (giver, gived, amount){
     await funcs.notify(gived, "Share", `${giver.user.tag} gave you **${amount.toLocaleString("en-US")} coins**`)
 })
     
