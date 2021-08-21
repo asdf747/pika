d8bb23c8dd2cef4d083ef37a2435ffcb38f3294c
confh <88879804+confh@users.noreply.github.com>
2021-08-21 20:32:04 +0200

    Update command-base.js

--- misc/command-base.js
+++ misc/command-base.js
@@ -63,6 +63,7 @@ module.exports = (client, commandOptions) => {
     permissions = [],
     permissionsbot = [],
     requiredRoles = [],
+    description = '',
     callback,
   } = commandOptions
 
@@ -244,8 +245,11 @@ if(dote > doc.Expire) doc.delete()
           message.lineReplyNoMention(
             new Discord.MessageEmbed()
       .setAuthor(message.author.username, message.author.displayAvatarURL())
-      .setColor(15158332)
-      .setDescription(`:x: Incorrect Usage of command: Missing Components\n\n \`${prefixx}${alias} ${expectedArgs}\`\n\n${subCommands.length ? `**Sub commands:** ${subCommands.split(/[ ]+/).map(a => `\`${a}\``).join(' ')}` : ''}`)
+      .setColor("RED")
+      .setTimestamp()
+      .setFooter(message.guild.name)
+      .setThumbnail(message.guild.iconURL() || null)
+      .setDescription(`:x: Missing arguments please use the syntax below.\n\n\`\`\`${prefixx}${alias} ${expectedArgs}\`\`\`\n\n${description !== '' ? `**Description:**\n${description}\n\n` : ''}${subCommands.length ? `**Sub commands:**\n${subCommands.split(/[ ]+/).map(a => `\`${a}\``).join(' ')}\n\n` : ''}${permissions.length ? `**Required Permissions:**\n${permissions[0]}\n\n` : ''}${typeof commands === 'string' ? "" : `**Aliases:**\n${commands.map(lol => `\`${lol}\``)}`}`)
           )
           return
         }
