5ad954026b9667c83111817e8b065f682521d419
confh <88879804+confh@users.noreply.github.com>
2021-08-21 18:52:26 +0200

    a

--- misc/calculate.js
+++ misc/calculate.js
@@ -1,13 +1,19 @@
 const disbut = require('discord-buttons');
- const { Calculator } = require('weky')
+const { calculate } = require('easy-calculation')
 const { MessageButton } = require('discord-buttons');
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
   commands: ['calculate', 'calc', 'cal'],
   description: 'Solves a math equation.',
-  disabled: true,
   callback: async (message, arguments, text, client) => {
-    if(message.channel.id === "818842687460343848") return;
-    await Calculator(message);
+    message.channel.send(
+      new MessageEmbed()
+        .setTitle("Calculator")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setThumbnail(message.guild.iconURL() || null)
+        .setDescription(`**Input:**\n\`\`\`${arguments.slice(0).join(' ')}\`\`\`\n\n**Output:**\n\`\`\`${calculator.calculate(arguments.slice(0).join(' '), true)}\`\`\``)
+        .setTimestamp()
+    )
   }
 }
