73425c22b364e4437e30fbdb9702b0102f97951b
confh <88879804+confh@users.noreply.github.com>
2021-08-21 15:09:40 +0200

    a

--- economyEvents/share.js
+++ economyEvents/share.js
@@ -1,6 +1,6 @@
 const funcs = require('../funcs')
 
-module.exports = (client, giver, gived, amount) => {
+module.exports = async (client, giver, gived, amount) => {
     await funcs.notify(gived, "Share", `${giver.user.tag} gave you **${amount.toLocaleString("en-US")} coins**`)
 
 }
