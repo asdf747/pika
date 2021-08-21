6dafc2cd42fd607a7778d6d8b981ec7749483a14
confh <88879804+confh@users.noreply.github.com>
2021-08-21 15:00:28 +0200

    Update economyEvents.js

--- economyEvents.js
+++ economyEvents.js
@@ -1,5 +1,5 @@
 const funcs = require('./funcs')
 
-client.economy.on('share', function (giver, gived, amount){
+client.economy.on('share', async function (giver, gived, amount){
     await funcs.notify(gived, "Share", `${giver.user.tag} gave you **${amount.toLocaleString("en-US")} coins**`)
 })
