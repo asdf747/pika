627130994a71fd7c15db223e42ade4fc4f1579cc
confh <88879804+confh@users.noreply.github.com>
2021-08-22 21:30:56 +0200

    Update funcs.js

--- funcs.js
+++ funcs.js
@@ -95,8 +95,7 @@ async function set(key, value){
 
 async function fetch(key){
     let modl = require('./models/jsons')
-    let cock =  modl.findOne({ ID: key })
-    return cock.Data
+    let cock =  modl.findOne({ ID: key }).then(e => {return e.data})
 }
 
 module.exports = {
