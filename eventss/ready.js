28d5c80265a36f4e8f3ccfb43569f740f285ef74
confh <88879804+confh@users.noreply.github.com>
2021-08-23 12:34:16 +0200

    Update ready.js

--- eventss/ready.js
+++ eventss/ready.js
@@ -14,9 +14,16 @@ const loadCommands11 = require('../google/load-commands')
 const Discord = require('discord.js')
 const schedule = require('node-schedule')
 const TEMP = require('../models/tempban')
+const db = require('../funcs')
+const no = require('../models/jsons')
 
 module.exports = async (client) => {
-  
+  await no.find(e => e.ID.startsWith('prefix_')).forEach(prefix => {
+    let nop = client.prefixes.get(prefix.Guild) || []
+    nop.unshift({
+      Prefix: prefix.Data
+    })
+  })
 
     console.log(`Logged in as ${client.user.username} (${client.user.id})`)
     client.user.setPresence({
