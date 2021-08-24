e553480f202499052065e6954539e1bda2b52360
confh <88879804+confh@users.noreply.github.com>
2021-08-24 11:13:41 +0200

    Update work.js

--- google/work.js
+++ google/work.js
@@ -16,16 +16,57 @@ module.exports = {
         
         bonus = await db.fetch(client, `bonus_work_${message.author.id}`)
         let amount = Math.floor(Math.random() * 15000) + 1 * bonus
+        let prizes = []
+        let emoji = ''
+        for (let i = 0; i < 100; i++){
+            prizes.push('nothing')
+        }
+        for (let i = 0; i < 30; i++){
+            prizes.push('PP')
+        }
+        for (let i = 0; i < 10; i++){
+            prizes.push('Lucky crate')
+        }
+        let prize = prizes[Math.floor(Math.random() * prizes.length)]
+        let count = Math.floor(Math.random() * 2) + 1
+        if(prize === 'Lucky crate') emoji = '<:emoji_12:877912311719927839>'
+        if(prize === 'PP') emoji = '<:penis:877589942140813333>'
         await economy.findOne({ id: message.author.id }, async(err, data) => {
             if(data){
                 await economy.findOneAndUpdate({ id: message.author.id }, { $inc: {Wallet: amount} })
-                db.set(client, `last_work_${message.author.id}`, Date.now())
+                switch(prize){
+                    case "PP":
+                        if(data.Inventory.find(tem => tem.Name === 'PP') && data.Inventory.find(tem => tem.Name === 'PP').Count >= 1){
+                            await economy.updateOne({ "id": message.author.id, "Inventory.Name": "PP" }, { $inc: {"Inventory.$.Count": count} })
+                        }else{
+                            let obj = {
+                                Name: "PP",
+                                Count: count
+                            }
+                            data.Inventory.push(obj)
+                            data.save()
+                        }
+                        break
+                    case "Lucky crate":
+                        if(data.Inventory.find(tem => tem.Name === 'Lucky crate') && data.Inventory.find(tem => tem.Name === 'Lucky crate').Count >= 1){
+                            await economy.updateOne({ "id": message.author.id, "Inventory.Name": "Lucky crate" }, { $inc: {"Inventory.$.Count": count} })
+                        }else{
+                            let ob = {
+                                Name: "Lucky crate",
+                                Count: count
+                            }
+                            data.Inventory.push(ob)
+                            data.save()
+                        }
+                        break
+                }
+                await db.set(client, `last_work_${message.author.id}`, Date.now())
                 return message.channel.send(
                     new MessageEmbed()
                     .setTitle("You've worked")
                     .setColor("BLUE")
                     .setTimestamp()
-                    .setDescription(`You've worked and got **${parseInt(amount).toLocaleString("en-US")}**\n${bonus !== 1 ? `Bonus: **${bonus.toLocaleString("en-Us")}**` : ''}`)
+                    .setDescription(`You've worked and got **${parseInt(amount).toLocaleString("en-US")}**${prize !== 'nothing' ? ` and ${count} ${prize}${count !== 1 ? 's': ''}` : ''}\n${bonus !== 1 ? `Bonus: **${bonus.toLocaleString("en-US")}**` : ''}`)
                 )
                 
             }if(!data){
@@ -36,13 +77,39 @@ module.exports = {
                     InBank: 0
                 }).save().then(async () => {
                     await economy.findOneAndUpdate({ id: message.author.id }, { $inc: {Wallet: amount} })
-                    db.set(client, `last_work_${message.author.id}`, Date.now())
+                    switch(prize){
+                        case "PP":
+                            if(data.Inventory.find(tem => tem.Name === 'PP') && data.Inventory.find(tem => tem.Name === 'PP').Count >= 1){
+                                await economy.updateOne({ "id": message.author.id, "Inventory.Name": "PP" }, { $inc: {"Inventory.$.Count": count} })
+                            }else{
+                                let obj = {
+                                    Name: "PP",
+                                    Count: count
+                                }
+                                data.Inventory.push(obj)
+                                data.save()
+                            }
+                            break
+                        case "Lucky crate":
+                            if(data.Inventory.find(tem => tem.Name === 'Lucky crate') && data.Inventory.find(tem => tem.Name === 'Lucky crate').Count >= 1){
+                                await economy.updateOne({ "id": message.author.id, "Inventory.Name": "Lucky crate" }, { $inc: {"Inventory.$.Count": count} })
+                            }else{
+                                let ob = {
+                                    Name: "Lucky crate",
+                                    Count: count
+                                }
+                                data.Inventory.push(ob)
+                                data.save()
+                            }
+                            break
+                    }
+                    await db.set(client, `last_work_${message.author.id}`, Date.now())
                 return message.channel.send(
                     new MessageEmbed()
                     .setTitle("You've worked")
                     .setColor("BLUE")
                     .setTimestamp()
-                    .setDescription(`You've worked and got **${parseInt(amount).toLocaleString("en-US")}**\n${bonus !== 1 ? `Bonus: **${bonus.toLocaleString("en-Us")}**` : ''}`)
+                    .setDescription(`You've worked and got **${parseInt(amount).toLocaleString("en-US")}**${prize !== 'nothing' ? ` and ${count} ${prize}${count !== 1 ? 's': ''}` : ''}\n${bonus !== 1 ? `Bonus: **${bonus.toLocaleString("en-US")}**` : ''}`)
                 )
                 })
             }
