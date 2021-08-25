2330b4fd38cd258a50682ed43bf675d81c0508dc
confh <88879804+confh@users.noreply.github.com>
2021-08-25 08:48:42 +0200

    a

--- owner/lb.js
+++ owner/lb.js
@@ -20,6 +20,7 @@ module.exports = {
       .setTitle("Donations leaderboard.")
       .setDescription(lb)
       .setFooter(message.guild.name, message.guild.iconURL())
+      .setColor("BLUE")
     )
   }
 }
--- the-poor-guild-only/muterole.js
+++ the-poor-guild-only/muterole.js
@@ -5,16 +5,16 @@ module.exports = {
   permissions: 'ADMINISTRATOR',
   callback: async (message, arguments, text, client) => {
     let role = message.guild.roles.cache.get(arguments[0])
-    if(arguments[0].startsWith('<@&') && arguments[0].endsWith('>')){
-      num = arguments[0].replace('<@&','').replace('>','')
+    if (arguments[0].startsWith('<@&') && arguments[0].endsWith('>')) {
+      num = arguments[0].replace('<@&', '').replace('>', '')
       role = message.guild.roles.cache.get(num)
     }
-    if(!role) return message.channel.send("This role doesn't exist.")
+    if (!role) return message.channel.send("This role doesn't exist.")
     message.guild.channels.cache.forEach((channel) => {
-      if(channel.type === 'text'){
-      channel.updateOverwrite(role, {
-            SEND_MESSAGES: false
-            })
+      if (channel.type === 'text') {
+        channel.updateOverwrite(role, {
+          SEND_MESSAGES: false
+        })
       }
     })
     await message.channel.send(":white_check_mark: | Progress done.")
