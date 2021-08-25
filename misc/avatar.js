4054a6a0c51ddfd995e2bc103e136de5e7714344
confh <88879804+confh@users.noreply.github.com>
2021-08-25 09:18:47 +0200

    a

--- misc/avatar.js
+++ misc/avatar.js
@@ -6,21 +6,21 @@ module.exports = {
   callback: async (message, arguments, text, client) => {
     const { guild } = message
     let member = 0
-    if(arguments[0]){
-    let maop = message.mentions.members.map(a => a)
-    if(maop.length) member = message.mentions.members.first()
-    if(Number(arguments[0])) member = guild.members.cache.get(arguments[0])
-    if(isNaN(arguments[0])) member = guild.members.cache.find(m => m.user.username.toLowerCase().includes(arguments.slice(0).join(' ')))
-    if(!member) member = guild.members.cache.find(m => m.displayName.toLowerCase().includes(arguments.slice(0).join(' ')))
-    if(!member) member = guild.members.cache.find(m => m.user.tag.toLowerCase() === arguments.slice(0).join(' ').toLowerCase())
+    if (arguments[0]) {
+      let maop = message.mentions.members.map(a => a)
+      if (maop.length) member = message.mentions.members.first()
+      if (Number(arguments[0])) member = guild.members.cache.get(arguments[0])
+      if (isNaN(arguments[0])) member = guild.members.cache.find(m => m.user.username.toLowerCase().includes(arguments.slice(0).join(' ')))
+      if (!member) member = guild.members.cache.find(m => m.displayName.toLowerCase().includes(arguments.slice(0).join(' ')))
+      if (!member) member = guild.members.cache.find(m => m.user.tag.toLowerCase() === arguments.slice(0).join(' ').toLowerCase())
     }
-    
-    if(!member) member = message.member
+
+    if (!member) member = message.member
     let av = member.user.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 })
     const avatar = new MessageEmbed()
-    .setTitle(`${member.user.username}'s avatar`)
-    .setImage(av)
-    .setColor(member.displayHexColor)
+      .setTitle(`${member.user.username}'s avatar`)
+      .setImage(av)
+      .setColor(member.displayHexColor)
 
     message.lineReplyNoMention(avatar)
   }
--- misc/embed.js
+++ misc/embed.js
@@ -9,28 +9,28 @@ module.exports = {
   expectedArgs: '<channel> <hex_code/random> <title>',
   callback: async (message, arguments, text, client) => {
     const chan = arguments[0].replace('<#', '').replace('>', '')
-const chck = message.guild.channels.cache.get(chan)
-if(!chck) return message.lineReplyNoMention("Invalid channel.")
-const me = message
+    const chck = message.guild.channels.cache.get(chan)
+    if (!chck) return message.lineReplyNoMention("Invalid channel.")
+    const me = message
 
-if(arguments[1].startsWith('#')){
-if(arguments[1].length > 7) return message.lineReplyNoMention("Invalid hex code.")
-if(arguments[1].length < 7) return message.lineReplyNoMention("Invalid hex code.")
-const emb = new MessageEmbed()
-.setTitle(`${arguments.slice(2).join(" ")}`)
-.setColor(`${arguments[1]}`)
+    if (arguments[1].startsWith('#')) {
+      if (arguments[1].length > 7) return message.lineReplyNoMention("Invalid hex code.")
+      if (arguments[1].length < 7) return message.lineReplyNoMention("Invalid hex code.")
+      const emb = new MessageEmbed()
+        .setTitle(`${arguments.slice(2).join(" ")}`)
+        .setColor(`${arguments[1]}`)
 
-chck.send(emb)
-.then(me.react("👍"))
-}
-else if(arguments[1].toLowerCase() === "random"){
-const emb = new MessageEmbed()
-.setTitle(`${arguments.slice(2).join(" ")}`)
-.setColor(`${arguments[1].toUpperCase()}`)
+      chck.send(emb)
+        .then(me.react("👍"))
+    }
+    else if (arguments[1].toLowerCase() === "random") {
+      const emb = new MessageEmbed()
+        .setTitle(`${arguments.slice(2).join(" ")}`)
+        .setColor(`${arguments[1].toUpperCase()}`)
 
-chck.send(emb)
-.then(me.react("👍"))
-}
-else return message.lineReplyNoMention("Invalid hex code.")
+      chck.send(emb)
+        .then(me.react("👍"))
+    }
+    else return message.lineReplyNoMention("Invalid hex code.")
   }
 }
