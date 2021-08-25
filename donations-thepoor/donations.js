f16fe0063a66134732635ff7a5e2a75b7ffb1951
confh <88879804+confh@users.noreply.github.com>
2021-08-25 16:21:12 +0200

    Update donations.js

--- donations-thepoor/donations.js
+++ donations-thepoor/donations.js
@@ -13,22 +13,22 @@ module.exports = {
   callback: async (message, arguments, text, client) => {
     const { guild } = message
     let member = message.guild.members.cache.find(m => m.id === arguments[0])
-    if(!Number(arguments[0])) member = guild.members.cache.find(m => m.user.username.toLowerCase().includes(arguments.slice(0).join(' ')))
-    if(!member) member = guild.members.cache.find(m => m.displayName.toLowerCase().includes(arguments.slice(0).join(' ')))
-    if(!member) member = message.mentions.members.first()
-    if(!member) return message.channel.send("I can't find this member.")
-    
+    if (!Number(arguments[0])) member = guild.members.cache.find(m => m.user.username.toLowerCase().includes(arguments.slice(0).join(' ')))
+    if (!member) member = guild.members.cache.find(m => m.displayName.toLowerCase().includes(arguments.slice(0).join(' ')))
+    if (!member) member = message.mentions.members.first()
+    if (!member) return message.channel.send("I can't find this member.")
+
     sche.findOne({ Guild: message.guild.id, User: member.user.id }, async (err, data) => {
-      if(data){
-        if(!data.Donations.length) return message.channel.send(
+      if (data) {
+        if (!data.Donations.length) return message.channel.send(
           new MessageEmbed()
-          .setAuthor(message.author.username, message.author.displayAvatarURL())
-          .setDescription(`:x: This user doesn't have any donation logged in this server.`)
-          .setColor(15158332)
+            .setAuthor(message.author.username, message.author.displayAvatarURL())
+            .setDescription(`:x: This user doesn't have any donation logged in this server.`)
+            .setColor(15158332)
         )
         const thing = data.Donations.reverse().map(
-          (w, i) => 
-          `**${i + 1}. Logger:** ${w.Logger}\n**Donation:** ${w.Donation}\nLogged on ${moment(w.Date).format('LLLL')} [${moment(w.Date, "YYYYMMDD").fromNow()}]`
+          (w, i) =>
+            `**${i + 1}. Logger:** ${w.Logger}\n**Donation:** ${w.Donation}\nLogged on ${moment(w.Date).format('LLLL')} [${moment(w.Date, "YYYYMMDD").fromNow()}]`
         )
         let options = {
           perPage: 7,
@@ -39,13 +39,13 @@ module.exports = {
           color: "BLUE"
         }
         embedPages(client, message, thing, options)
-      } if(!data){
+      } if (!data) {
         message.channel.send(
           new MessageEmbed()
-          .setAuthor(message.author.username, message.author.displayAvatarURL())
-          .setTitle("No donations yet")
-          .setDescription(`:x: This user doesn't have any donation logged in this server.`)
-          .setColor(15158332)
+            .setAuthor(message.author.username, message.author.displayAvatarURL())
+            .setTitle("No donations yet")
+            .setDescription(`:x: This user doesn't have any donation logged in this server.`)
+            .setColor(15158332)
         )
       }
     })
