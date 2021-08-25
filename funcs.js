25deaea12a5cc65d6b03ba943c157ee34e88586e
confh <88879804+confh@users.noreply.github.com>
2021-08-25 16:03:29 +0200

    a

--- funcs.js
+++ funcs.js
@@ -230,7 +230,6 @@ async function embedPages(client, message, array, options) {
     if(timestamp) embed.setTimestamp()
     let msg = await message.channel.send(embed)
     if(array.length > perPage){
-    if(options.args) return
     await msg.react(emojis.first_track)
     await msg.react(emojis.previous_track)
     if (args === false && Math.ceil(array.length / perPage) > 3) await msg.react(emojis.jump_to)
--- the-poor-guild-only/mydonatons.js
+++ the-poor-guild-only/mydonatons.js
@@ -2,6 +2,7 @@ const sche = require('../models/donations')
 const { MessageEmbed } = require('discord.js')
 const Pagination = require('discord-paginationembed');
 const moment = require('moment')
+const { embedPages } = require('../funcs')
 
 module.exports = {
   commands: ['mydonations', 'mydonation', 'mydonos', 'mydono'],
@@ -9,36 +10,33 @@ module.exports = {
   guildID: '655780171496030240',
   callback: async (message, arguments, text, client) => {
     const member = message.member
-    
+
     sche.findOne({ Guild: message.guild.id, User: member.user.id }, async (err, data) => {
-      if(data){
-        if(!data.Donations.length) return message.channel.send( new MessageEmbed()
+      if (data) {
+        if (!data.Donations.length) return message.channel.send(new MessageEmbed()
           .setAuthor(message.author.username, message.author.displayAvatarURL())
           .setDescription(`:x: You don't have any donation logged in this server.`)
           .setColor(15158332))
         const thing = data.Donations.map(
-          (w, i) => 
-          `\`${i + 1}\` | **Logger:** ${w.Logger}\n**Donation:** ${w.Donation}\nLogged on ${moment(w.Date).format('LLLL')} [${moment(w.Date, "YYYYMMDD").fromNow()}]`
+          (w, i) =>
+            `\`${i + 1}\` | **Logger:** ${w.Logger}\n**Donation:** ${w.Donation}\nLogged on ${moment(w.Date).format('LLLL')} [${moment(w.Date, "YYYYMMDD").fromNow()}]`
         )
-    const FieldsEmbed = new Pagination.FieldsEmbed()
-    .setArray(thing)
-    .setAuthorizedUsers([message.author.id])
-    .setChannel(message.channel)
-    .setElementsPerPage(8)
-    .setPageIndicator(true)
-    .formatField(`${data.Donations.length} donations.`, el => el);
 
-FieldsEmbed.embed
-  .setTitle(`${member.user.tag}'s donations.`)
-  .setColor(65535)
-  .setFooter(`ID: ${member.user.id}`)
-
-FieldsEmbed.build();
-      } if(!data){
-        message.channel.send( new MessageEmbed()
-          .setAuthor(message.author.username, message.author.displayAvatarURL())
-          .setDescription(`:x: You don't have any donation logged in this server.`)
-          .setColor(15158332))
+        let options = {
+          perPage: 7,
+          joinBy: '\n\n',
+          color: 65535,
+          footer: `ID: ${member.user.id}`,
+          title: `${member.user.tag}'s donations.`,
+          header: `${data.Donations.length} donations.\n`
+        }
+        embedPages(client, message, thing, options)
+      } if (!data) {
+        message.channel.send(
+          new MessageEmbed()
+            .setAuthor(message.author.username, message.author.displayAvatarURL())
+            .setDescription(`:x: You don't have any donation logged in this server.`)
+            .setColor(15158332))
       }
     })
   }
