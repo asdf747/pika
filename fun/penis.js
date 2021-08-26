ce94ecb3f40b20ce14d4601a0812951e4a76ebbd
confh <88879804+confh@users.noreply.github.com>
2021-08-26 11:39:51 +0200

    a

--- fun/penis.js
+++ fun/penis.js
@@ -4,13 +4,13 @@ module.exports = {
   commands: ['pp', 'penis'],
   callback: async (message, arguments, text, client) => {
     const user = message.mentions.users.first() || message.author;
-        const pps = ["=","==","===","====","=====","======","=======","========"];
-        const pp = pps[Math.floor(Math.random() * pps.length)];
-        const ppsize = new MessageEmbed()
-        .setTitle("PP size machine")
-        .setDescription(`${user.tag}'s penis\n8${pp}D`)
-        .setColor("RANDOM")
-message.channel.send(ppsize)
+    const pps = ["=", "==", "===", "====", "=====", "======", "=======", "========"];
+    const pp = pps[Math.floor(Math.random() * pps.length)];
+    const ppsize = new MessageEmbed()
+      .setTitle("PP size machine")
+      .setDescription(`${user.tag}'s penis\n8${pp}D`)
+      .setColor("RANDOM")
+    message.channel.send(ppsize)
 
   }
 }
