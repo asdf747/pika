932314e0d40b5c42abf4924c2d63bfa8febb2576
confh <88879804+confh@users.noreply.github.com>
2021-08-27 17:46:25 +0200

    Update script.js

--- owner/script.js
+++ owner/script.js
@@ -3,8 +3,9 @@ const db = require('../funcs')
 module.exports = {
     commands: 'script',
     callback: async (message, arguments, text, client) => {
-        let func = function gg(){
-            return message.channel.send('no')
+        let func = function () {
+
+            console.log('e')
         }
         let ass = func.toString()
         let non = new Function(ass)
