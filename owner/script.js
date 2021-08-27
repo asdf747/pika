7a136afc13a9b1126ac5847e377e9534567c1320
confh <88879804+confh@users.noreply.github.com>
2021-08-27 17:44:01 +0200

    Update script.js

--- owner/script.js
+++ owner/script.js
@@ -3,9 +3,11 @@ const db = require('../funcs')
 module.exports = {
     commands: 'script',
     callback: async (message, arguments, text, client) => {
-        let action = async function(){
-            message.channel.send('test')
+        let func = function gg(){
+            return message.channel.send('no')
         }
-        await db.set(client, 'test', action)
-    }
+        let ass = func.toString()
+        let non = new Function(ass)
+        non()
+}
 }
