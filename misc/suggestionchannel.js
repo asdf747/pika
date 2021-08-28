8c1ef3f13d02ed25ed3d0e75e57f5717370b8aa9
confh <88879804+confh@users.noreply.github.com>
2021-08-28 18:07:10 +0200

    Update suggestionchannel.js

--- misc/suggestionchannel.js
+++ misc/suggestionchannel.js
@@ -9,15 +9,16 @@ module.exports = {
         let channel = message.mentions.channels.first()
         if(!channel && arguments[0] !== 'none') return message.channel.send("That channel doesn't exist")
         if(arguments[0] === 'none') channel = 'null'
+        if(channel) channel = channel.id
         await suggest.findOne({ Guild: message.guild.id }, async(err, data) => {
             if(data){
-                await suggest.findOne({ Guild: message.guild.id }, { $set: {Channel: `${channel ? channel.id : 'null'}`} })
+                await suggest.findOne({ Guild: message.guild.id }, { $set: {Channel: channel} })
                 message.channel.send(`Set the suggestion channel to **${channel ? channel.toString : 'none'}**`)
             }if(!data){
                 if(channel === 'null') return message.channel.send("The suggestion channel isn't even set")
                 await new suggest({
                     Guild: message.guild.id,
-                    Channel: `${channel ? channel.id : 'null'}`
+                    Channel: channel
                 }).save()
                 message.channel.send(`Set the suggestion channel to **${channel ? channel.toString() : 'none'}**`)
             }
