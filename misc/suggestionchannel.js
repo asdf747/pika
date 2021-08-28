3101640302ad4e23e5448b155be476af84d5ae2c
confh <88879804+confh@users.noreply.github.com>
2021-08-28 18:19:12 +0200

    Update suggestionchannel.js

--- misc/suggestionchannel.js
+++ misc/suggestionchannel.js
@@ -7,20 +7,20 @@ module.exports = {
     expectedArgs: "<channel/none>",
     callback: async (message, arguments, text, client) => {
         let channel = message.mentions.channels.first()
-        if(!channel && arguments[0] !== 'none') return message.channel.send("That channel doesn't exist")
-        if(arguments[0] === 'none') channel = 'null'
-        if(channel) channel = channel.id
-        await suggest.findOne({ Guild: message.guild.id }, async(err, data) => {
-            if(data){
-                await suggest.findOneAndUpdate({ Guild: message.guild.id }, { $set: {Channel: channel} })
-                message.channel.send(`Set the suggestion channel to **${channel ? channel.toString : 'none'}**`)
-            }if(!data){
-                if(channel === 'null') return message.channel.send("The suggestion channel isn't even set")
+        if (!channel && arguments[0] !== 'none') return message.channel.send("That channel doesn't exist")
+        if (arguments[0] === 'none') channel = 'null'
+        if (channel) channel = channel.id
+        await suggest.findOne({ Guild: message.guild.id }, async (err, data) => {
+            if (data) {
+                await suggest.findOneAndUpdate({ Guild: message.guild.id }, { $set: { Channel: channel } })
+                message.channel.send(`Set the suggestion channel to **${Number(channel) ? `<#${channel}>` : 'none'}**`)
+            } if (!data) {
+                if (channel === 'null') return message.channel.send("The suggestion channel isn't even set")
                 await new suggest({
                     Guild: message.guild.id,
                     Channel: channel
                 }).save()
-                message.channel.send(`Set the suggestion channel to **${channel ? channel.toString() : 'none'}**`)
+                message.channel.send(`Set the suggestion channel to **${Number(channel) ? `<#${channel}>` : 'none'}**`)
             }
         })
     }
